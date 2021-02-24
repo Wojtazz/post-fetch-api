253623e074a466000e112c0042842647d896dd1f
Wojciech Wesołowski <wwesolowski97@gmail.com>
2021-02-24 18:44:52 +0100

    App dto

---
+++ src/main/java/com/wwesolowski/postfetchapi/dto/GetAllPostsDto.java
@@ -0,0 +1,14 @@
+package com.wwesolowski.postfetchapi.dto;
+
+public class GetAllPostsDto {
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/main/java/com/wwesolowski/postfetchapi/dto/UpdatePostDto.java
@@ -0,0 +1,24 @@
+package com.wwesolowski.postfetchapi.dto;
+
+public class UpdatePostDto {
+
+    private String title;
+
+    private String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
