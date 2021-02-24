497c5ed7a89fec09673561916c80c2eb56443e2d
Wojciech Wesołowski <wwesolowski97@gmail.com>
2021-02-24 18:03:18 +0100

    Project init

---
+++ src/main/java/com/wwesolowski/postfetchapi/PostFetchApiApplication.java
@@ -0,0 +1,13 @@
+package com.wwesolowski.postfetchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostFetchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostFetchApiApplication.class, args);
+	}
+
+}
